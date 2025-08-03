4ecec1a266a51226d2818c53c6c87f244053b26d
Nidhi <nidhii200107@gmail.com>
2025-08-04 00:09:58 +0530

    commit changes in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
-//import authRoutes from "./routes/auth.route.js";
-//import messageRoutes from "./routes/message.route.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -25,8 +25,8 @@ app.use(
   })
 );
 
-//app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
