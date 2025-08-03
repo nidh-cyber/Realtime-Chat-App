e0772b48e2084837eb7ffe9f5e27915c564ed103
Nidhi <nidhii200107@gmail.com>
2025-08-03 23:35:24 +0530

    comment out message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+// app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
